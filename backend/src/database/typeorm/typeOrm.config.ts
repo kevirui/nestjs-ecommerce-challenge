cb8762ca2f51e786ddeada603b858778641d3e2d
kevirui <kevinagustinrockz@gmail.com>
2026-03-19 20:02:33 -0300

    fix build render

--- backend/src/database/typeorm/typeOrm.config.ts
+++ backend/src/database/typeorm/typeOrm.config.ts
@@ -9,11 +9,11 @@ const envFilePath: string = getEnvPath(
 config({ path: envFilePath });
 export const dataSourceOptions: DataSourceOptions = {
   type: 'postgres',
-  host: process.env.DATABASE_HOST,
-  port: parseInt(process.env.DATABASE_PORT, 10),
-  database: process.env.DATABASE_NAME,
-  username: process.env.DATABASE_USER,
-  password: process.env.DATABASE_PASSWORD,
+  host: process.env.DATABASE_HOST || 'localhost',
+  port: parseInt(process.env.DATABASE_PORT || '5432', 10),
+  database: process.env.DATABASE_NAME || 'ecommercedb',
+  username: process.env.DATABASE_USER || 'hassan',
+  password: process.env.DATABASE_PASSWORD || 'password',
   entities: [process.env.DATABASE_ENTITIES],
   migrations: ['dist/database/migration/history/*.js'],
   logger: 'simple-console',
